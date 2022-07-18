584711468b7d13c26f04cc9828b102701e171423
Shaz Momin <shaz.momin@utexas.edu>
2022-07-18 15:26:01 -0500

    card ui setup

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Card = () => {
-  return (
-    <div>
-        Card
-    </div>
-  )
+    const [date, setDate] = useState(new Date().toLocaleDateString())
+    const [data, setData] = useState({
+        term: "blockchain",
+        grammar: "noun",
+        definition: "a system in which a record of transactions made in bitcoin or another cryptocurrency are maintained across several computers that are linked in a peer-to-peer network"
+    })
+    const options = {weekday: 'long', day: '2-digit', year: 'numeric', month: 'long' }
+
+    useEffect(() => {
+        setDate(new Date())
+    }, [])
+    
+
+    return (
+        <div class="bg-white border-none p-5 rounded-xl h-auto ml-[6%] mr-[6%] md:mx-0 items-center sm:max-w-sm shadow-md">
+            <div class="text-center text-md text-gray-600 tracking-wide">{date.toLocaleString('en-US', options).replace(/,/,"")}</div>
+            <div class="p-8 text-center">
+                <div class="text-4xl font-semibold text-indigo-800">{data.term}</div>
+                <div class="italic font-semibold text-emerald-700">{data.grammar}</div>
+            </div>
+            <div class="text-lg tracking-wide font-light px-8 pb-6 text-center">{data.definition}</div>
+        </div>
+    )
 }
 
 export default Card
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Card from '../components/Card'
 
 export default function Home() {
+
   return (
-    <div class="w-full h-screen bg-gray-200">
-      Hello World
+    <div class="main-bg w-full h-screen bg-gray-200 grid justify-center content-center">
+      <Card />
     </div>
   )
 }
