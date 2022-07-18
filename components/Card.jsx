8906f0e9fa93d5146438cafe4a76eadb97257b1c
Shaz Momin <shaz.momin@utexas.edu>
2022-07-18 17:03:40 -0500

    setting up terms

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
-const Card = () => {
+const Card = ({ dat }) => {
     const [date, setDate] = useState(new Date().toLocaleDateString())
     const [data, setData] = useState({
         term: "blockchain",
@@ -11,15 +11,14 @@ const Card = () => {
 
     useEffect(() => {
         setDate(new Date())
-    }, [])
-    
+    }, [])    
 
     return (
-        <div class="bg-white border-none p-5 rounded-xl h-auto ml-[6%] mr-[6%] md:mx-0 items-center sm:max-w-sm shadow-md">
-            <div class="text-center text-md text-gray-600 tracking-wide">{date.toLocaleString('en-US', options).replace(/,/,"")}</div>
-            <div class="p-8 text-center">
-                <div class="text-4xl font-semibold text-indigo-800">{data.term}</div>
-                <div class="italic font-semibold text-emerald-700">{data.grammar}</div>
+        <div className="bg-white border-none p-5 rounded-xl h-auto ml-[6%] mr-[6%] md:mx-0 items-center sm:max-w-sm shadow-md">
+            <div className="text-center text-md text-gray-600 tracking-wide">{date.toLocaleString('en-US', options).replace(/,/,"")}</div>
+            <div className="p-8 text-center">
+                <div className="text-4xl font-semibold text-indigo-800">{data.term}</div>
+                <div className="italic font-semibold text-emerald-700">{data.grammar}</div>
             </div>
             <div class="text-lg tracking-wide font-light px-8 pb-6 text-center">{data.definition}</div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Card from '../components/Card'
+import { useEffect } from 'react'
 
-export default function Home() {
+export default function Home({ data }) {
 
   return (
     <div class="main-bg w-full h-screen bg-gray-200 grid justify-center content-center">
-      <Card />
+      <Card data={data} />
     </div>
   )
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side
+export async function getServerSideProps() {
+
+  const index = Math.floor(Math.random() * 125);
+  const res = await fetch('http://localhost:3000/api/terms')
+  const data = await res.json()
+
+  return {
+    props: {
+      data
+    }
+  }
 }
