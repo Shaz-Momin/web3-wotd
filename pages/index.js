9f4b6c5e0153bdbe367f9556db09e65d1a05ad55
Shaz Momin <shaz.momin@utexas.edu>
2022-07-21 16:10:12 -0500

    meta tags updated

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,24 @@ import Card from '../components/Card'
 import { getDoc, doc, setDoc } from "firebase/firestore"; 
 import { db } from '../config/firebase'
 import { useState, useEffect } from "react"
+import DocumentMeta from 'react-document-meta';
+import Head from 'next/head'
+
 
 export default function Home() {
 
   const [data, setData] = useState({})
+  const meta = {
+    title: 'Web 3 - Word of the Day',
+    description: 'Explore Web-3 terminology everyday',
+    canonical: 'http://web3-wotd.vercel.app',
+    meta: {
+      charset: 'utf-8',
+      name: {
+        keywords: 'web3,web,word,terms,everday,technology,definition,glossary,vocabulary'
+      }
+    }
+  };
 
   useEffect(() => {
     const info = getData()
@@ -15,9 +29,16 @@ export default function Home() {
   }, [])
 
   return (
-    <div className="main-bg w-full h-screen bg-gray-200 grid justify-center content-center">
-      {data['term'] && <Card data={data} setData={setData} />}
-    </div>
+    <DocumentMeta {...meta}>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="theme-color" content="#311E5C" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌐</text></svg>" />
+      </Head>
+      <div className="main-bg w-full h-screen bg-gray-200 grid justify-center content-center">
+        {data['term'] && <Card data={data} setData={setData} />}
+      </div>
+    </DocumentMeta>
   )
 }
 
